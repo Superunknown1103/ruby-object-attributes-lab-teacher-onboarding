1f69c31100a4976bdf6127861c976a14e7d33c49
Bruce Tsvilikhovsky <bruce.tsvilikhovsky@flatironschool.com>
2019-03-28 15:02:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
 class Dog
   def name = (dogs_name)
-    # Instance variables
-    @name = dogs_name
+    # Instance variable
+      @name = dogs_name
   end
 
-  def #name
+  def name
+    @name
+  end
+
+  def breed = (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
   end
 end
